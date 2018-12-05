4f160db5192a7c4e98a6b6e17815d745f49f3204
Jacob <mwaniki.jacob@gmail.com>
2018-12-05 08:17:21 +0300

    add test edge cases, add base_case.py

--- app/tests/v1/test_views.py
+++ app/tests/v1/test_views.py
@@ -1,88 +1,121 @@
-import unittest
-import os
+
 import json
 
-from app import create_app
+#local imports
+from .Base_test import BaseTestCase
 
-INCIDENTS_URL = '/api/v1/red-flags'
-INCIDENT_URL = 'api/v1/red-flag/1'
-
-class test_IncidentsTestCases (unittest.TestCase):
-    #This represent the Red-flag test case
+class TestCreateflag(BaseTestCase):
     def setUp(self):
-        #test varriables to initialize app
-		self.client = self.app.test_client()
-        self.app = create_app(config_name='testing')
-        self.test_flag = {"flag_id":1, "Title":"Corruption in county finance office", "Comment":"dfffsfsfdsfsdfsdfdsfdfsdffdffdfdfffsdffffdf", "Location":"Thika"}
-        
-    def test_CreateFlag(self):
-        #Test API can create a Red-Flag (post request)
-        response=self.app.post(INCIDENTS_URL)
-        result=json.loads(response, data =self.Incidents)
-        self.assertEqual(result["msg"],"success")
-        self.assertEqual(response.status_code,201)
-        
+        #register a test user
+        BaseTestCase.setUp(self)
+
+    def tearDown(self):
+        BaseTestCase.tearDown(self)
+
+
+    def test_create_a_flag(self):
+        '''Test that a user can create an flag of type red-flag or intervention'''
+        response = self.client.post(
+            'api/v1/red-flags/',  data=json.dumps(self.redflag_data)
+        )
+        data = json.loads(response.data)
+
+        self.assertEqual(data['data'][0]['message'], 'Created flag record')
+        self.assertTrue(response.status_code == 200)
+
+    def test_get_all_flags(self):
+        '''Test that a user can get all the flags'''
+        response = self.client.get(
+             'api/v1/red-flags/', headers=self.headers
+        )
+        self.assertTrue(response.status_code == 200)
+
+    def test_get_a_flag(self):
+        '''Test user can get a specific flag given its id'''
+        self.client.post(
+            'api/v1/red-flag/',  data=json.dumps(self.redflag_data)
+        )
+        response = self.client.get(
+             'api/v1/red-flag/1', headers=self.headers
+        )
+        data = json.loads(response.data)
+
+        self.assertEqual(data['data'][0]['id'], 1)
+        self.assertTrue(response.status_code == 200)
 
     
-    def test_GetAllRedIncidents(self):
-        #Test API can get all existing Red-Flag (GET request)
-        response =self.app.post(INCIDENTS_URL)
-        result=json.loads(response, data=self.Incidents)
-        self.assertEqual(response.status_code,201)
-        response = self.app.Get("/Incidents/")
-        self.assertEqual(result["msg"],"success")
-        self.assertEqual(response.status_code,200)
-        self.assertIn("Corruption in county finance office", str(response.data))
+    def test_update_location(self):
 
-    def test_GetRedIncidentsById(self):
-        #Test get an existing Red-Flag by Id (GET request)
-        response =self.app.post(INCIDENTS_URL, data=self.Incidents)
-        self.assertEqual(response.status_code,201)
-        result_in_js=json.loads(response.data.decode('utf-8').replace("'","\"") )
-        result=self.app.Get(
-            "/Incidents/{}".format(result_in_js["flag_id"])
+        self.client.post(
+            'api/v1/red-flags/',  data=json.dumps(self.redflag_data)
         )
-        self.assertEqual(result.status_code, 200)
-        self.assertIn("Thika", str(result.data))
+        response = self.client.patch(
+             'api/v1/red-flags/1',  data=json.dumps(self.location_data)
+        )
 
-    def test_EditRedIncidentsById(self):
-        #Test API can edit an existing Red-Flag by Id (PUT request)
-        response =self.app.post(INCIDENTS_URL)
-        result=json.loads(response, data={
-                                        "flag_id":1,
-                                        "Title":"Corruption in county finance office",
-                                        "Comment":"dfffsfsfdsfsdfsdfdsfdfsdffdffdfdfffsdffffdf",
-                                        "Location":"Thika"  
-                                            })
-        self.assertEqual(result["msg"],"success")
-        self.assertEqual(response.status_code,201)
-        response =self.app.Patch("/flag/1")
-        rv =json.loads(response, data={"flag_id":1,
-                                        "Title":"Corruption in county finance office",
-                                        "Comment":"This comment has been updated",
-                                        "Location":"Thika"})
-        self.assertEqual(rv.status_code,200)
-        results= self.app.Get("/flag/1")
-        self.assertIn("This comment has been updated", str(results.data))
+        data = json.loads(response.data)
+        
+        self.assertEqual(data['message'], 'Updated red-flag record’s location')
+        self.assertTrue(response.status_code == 200)
 
+    def test_update_comment(self):
 
-    def test_DeleteRedFlagById(self):
-        #Test API can delete an existing Red-Flag by Id (DELETE request)
-        response =self.app.post(INCIDENT_URL)
-        result=json.loads(response, data={"flag_id":1,
-                                        "Title":"Corruption in county finance office",
-                                        "Comment":"This comment has been updated",
-                                        "Location":"Thika" } )
-        self.assertEqual(response.status_code,201)
-        res=self.app.Delete(INCIDENT_URL)
-        self.assertEqual(res.status_code, 200)
-        #Test if it still exists, should return 404
-        results=self.app.GET(INCIDENT_URL)
-        self.assertEqual(result.status_code, 404)
+        self.client.post(
+            'api/v1/red-flags/',  data=json.dumps(self.redflag_data)
+        )
 
-    def tearDown():
-        #teardown all initialized variables.
-        pass
+        response = self.client.patch(
+             'api/v1/red-flags/1/',  data=json.dumps(self.comment_data)
+        )
+        data = json.loads(response.data)
 
-if __name__ =="__main__":
-    unittest.main()
+        self.assertEqual(data['message'], 'Updated red-flag record’s comment')
+        self.assertTrue(response.status_code == 200)
+
+    def test_delete_flag(self):
+        self.client.post(
+            'api/v1/red-flags/',  data=json.dumps(self.redflag_data)
+        )
+        response = self.client.delete(
+             'api/v1/red-flags/1', headers=self.headers)
+
+        data = json.loads(response.data)
+
+        self.assertEqual(data['data'][0]['id'], 1)
+        self.assertTrue(response.status_code == 200)
+
+    def test_create_flag_with_empty_username(self):
+        response = self.client.post(
+            'api/v1/red-flags/',  data=json.dumps(self.redflag_data_with_empty_created)
+            )
+        data = json.loads(response.data)
+
+        self.assertEqual(data['message'], {'message': 'fields cannot be empty'})
+        self.assertTrue(response.status_code==400)
+        
+    def test_create_flag_with_empty_type(self):
+        response = self.client.post(
+            'api/v1/red-flags/',  data=json.dumps(self.redflag_data_with_empty_type)
+            )
+        data = json.loads(response.data)
+
+        self.assertEqual(data['message'], {'message': 'fields cannot be empty'})
+        self.assertTrue(response.status_code==400)
+
+    def test_create_flag_with_empty_location(self):
+        response = self.client.post(
+            'api/v1/red-flags/',  data=json.dumps(self.redflag_data_empty_location)
+            )
+        data = json.loads(response.data)
+
+        self.assertEqual(data['message'], {'message': 'fields cannot be empty'})
+        self.assertTrue(response.status_code==400)
+
+    def test_create_flag_with_empty_comment(self):
+        response = self.client.post(
+            'api/v1/red-flags/',  data=json.dumps(self.redflag_data_empty_comment)
+            )
+        data = json.loads(response.data)
+
+        self.assertEqual(data['message'], {'message': 'fields cannot be empty'})
+self.assertTrue(response.status_code==400)
